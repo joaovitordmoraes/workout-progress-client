e4fa5d1cd973443315e449fe4ad928cbc62e9d27
joaovitordmoraes <joaovitordmoraes@gmail.com>
2024-04-19 21:01:15 -0300

    Refactor: change location of provider folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import NextAuthSessionProvider from '@/provider/sessionProvider'
+import NextAuthSessionProvider from '@/app/provider/sessionProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
