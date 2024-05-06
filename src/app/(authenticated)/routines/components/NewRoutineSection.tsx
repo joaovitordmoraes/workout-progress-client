48c6045819a0caf906d8c67e51c60209ac2ee387
joaovitordmoraes <joaovitordmoraes@gmail.com>
2024-05-06 20:47:55 -0300

    Fix: remove alerts

--- src/app/(authenticated)/routines/components/NewRoutineSection.tsx
+++ src/app/(authenticated)/routines/components/NewRoutineSection.tsx
@@ -31,8 +31,6 @@ export function NewRoutineSection() {
         data.name,
       )
 
-      alert(createTrainingRoutineRequest)
-
       if (createTrainingRoutineRequest.success) {
         setUpdateRoutines(false)
         createRoutineForm.reset()
