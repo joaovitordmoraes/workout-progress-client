eb94e2c2207db5bc250bf96dc4403a8247e46158
joaovitordmoraes <joaovitordmoraes@gmail.com>
2024-05-06 21:06:52 -0300

    Feat: Update viewport settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import { Roboto } from 'next/font/google'
 import './globals.css'
 import NextAuthSessionProvider from '@/app/provider/sessionProvider'
@@ -11,6 +11,12 @@ const roboto = Roboto({
 export const metadata: Metadata = {
   title: 'Workout Progress',
   description: 'Acompanhe seu progresso nos treinos.',
+}
+
+export const viewport: Viewport = {
+  maximumScale: 1,
+  width: 'device-width',
+  initialScale: 1,
 }
 
 export default function RootLayout({
